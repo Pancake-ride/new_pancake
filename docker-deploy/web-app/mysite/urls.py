3fd5b2b467311720d3d441fd7a8126972115acd9
Qizhang-Feng <qf31@duke.edu>
2020-02-07 16:37:34 -0500

    debug

--- docker-deploy/web-app/mysite/urls.py
+++ docker-deploy/web-app/mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/login/', TemplateView.as_view(template_name='home.html'), name='home'),
     #path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('polls/', include('polls.urls')),
--- docker-deploy/web-app/pancakeride/views.py
+++ docker-deploy/web-app/pancakeride/views.py
@@ -5,11 +5,13 @@ from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Ride, Driver
+from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib import messages
 from django.views import generic
 from django.db.models import Q, F
 from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 
 from .forms import DriverRegisteForm, RideRequestForm, SharerSearchForm
 
@@ -22,6 +24,7 @@ def Driver_regist(request):
                 driver_info = get_object_or_404(Driver, pk = request.user)
             else:
                 driver_info = Driver.objects.create(user = request.user,capacity = 1)
+            
             driver_info.first_name = form.cleaned_data['first_name']
             driver_info.last_name = form.cleaned_data['last_name']
             driver_info.license_plate_number = form.cleaned_data['license_plate_number']
@@ -29,6 +32,15 @@ def Driver_regist(request):
             driver_info.special_vehicle_info = form.cleaned_data['special_vehicle_info']
             driver_info.vehicle_type = form.cleaned_data['vehicle_type']
             driver_info.save()
+            
+            driver_email = get_object_or_404(User, username = request.user).email
+            send_mail(
+                'Account Driver register for Pancake Ride',
+                'Congratulation '+ driver_info.first_name + ' '+ driver_info.last_name +'! You have successfully regist/edit driver info for Pnacake Ride.\n',
+                'Pancake Ride',
+                [driver_email],
+                fail_silently=False,
+            )
             return redirect('pancakeride:main_home')
     else:
         if Driver.objects.filter(user = request.user).exists():
@@ -58,6 +70,17 @@ def Ride_request(request):
             ride_info.special_vehicle_info = form.cleaned_data['special_vehicle_info']
             ride_info.shareable = form.cleaned_data['shareable']
 
+            user = get_object_or_404(User, username = request.user)
+            user_email = user.email
+            user_name = user.first_name + ' ' + user.last_name
+            send_mail(
+                'Account Driver register for Pancake Ride',
+                'Congratulation '+ user_name +'! You have successfully request a new ride in Pnacake Ride.\n',
+                'Pancake Ride',
+                [user_email],
+                fail_silently=False,
+            )
+
             ride_info.save()
             return redirect('pancakeride:main_home')
         else:
@@ -85,6 +108,18 @@ def Ride_request_edit(request, pk):
             ride_edit.special_vehicle_info = edit_form.cleaned_data['special_vehicle_info']
             ride_edit.shareable = edit_form.cleaned_data['shareable']
             ride_edit.save()
+
+            user = get_object_or_404(User, username = request.user)
+            user_email = user.email
+            user_name = user.first_name + ' ' + user.last_name
+            send_mail(
+                'Account Driver register for Pancake Ride',
+                'Congratulation '+ user_name +'! You have successfully edit a ride in Pnacake Ride.\n',
+                'Pancake Ride',
+                [user_email],
+                fail_silently=False,
+            )
+
             return redirect('pancakeride:main_home')
         else:
             return redirect('pancakeride:ride_request_edit', pk)
@@ -153,14 +188,39 @@ def Sharer_search(request):
 def Sharer_confirm(request, pk):
     ride_detail = get_object_or_404(Ride, pk = pk)
     if request.POST:
-        print('ppost')
+        #print('ppost')
         print(request.POST['sharer_num'])
         ride_detail.sharer_num = request.POST['sharer_num']
         ride_detail.sharer = request.user
         ride_detail.save()
+
+        owner = get_object_or_404(User, username = ride_detail.owner)
+        owner_email = owner.email
+        owner_name = owner.first_name + ' ' + owner.last_name
+
+        sharer = get_object_or_404(User, username = request.user)
+        sharer_email = sharer.email
+        sharer_name = sharer.first_name + ' ' + sharer.last_name
+
+        send_mail(
+            'Ride Sharer for Pancake Ride',
+            'Hi '+ owner_name +'! '+ sharer_name + ' has successfully joined your ride in Pnacake Ride.\n',
+            'Pancake Ride',
+            [owner_email],
+            fail_silently=False,
+        )
+
+        send_mail(
+            'Ride Sharer for Pancake Ride',
+            'Hi '+ sharer_name +'! You have successfully joined ' + owner_name + ' ride in Pnacake Ride.\n',
+            'Pancake Ride',
+            [sharer_email],
+            fail_silently=False,
+        )
+
         return redirect('pancakeride:main_home')
     else:
-        print('gget')
+        #print('gget')
         ride_detail = get_object_or_404(Ride, pk = pk)
         if ride_detail.owner != request.user:
             print('user Id error!')
@@ -169,16 +229,17 @@ def Sharer_confirm(request, pk):
 
 def Driver_search(request):
     if request.method == 'GET':
+
         context = {}
         if Driver.objects.filter(user = request.user).exists():
             print('ddrive')
             capacity = get_object_or_404(Driver, pk = request.user).capacity
             vehicle_type = get_object_or_404(Driver, pk = request.user).vehicle_type
-            available_rides = Ride.objects.annotate(i_sum=F('passenger_num') + F('sharer_num')).filter(i_sum__lte = capacity).filter(driver__isnull = True).exclude(owner = request.user).filter(vehicle_type__exact = vehicle_type)
+            available_rides = Ride.objects.annotate(i_sum=F('passenger_num') + F('sharer_num')).filter(i_sum__lte = capacity).filter(driver__isnull = True).exclude(owner = request.user).filter(vehicle_type__exact = vehicle_type).filter(status__exact = 'op')
             context['availabel_rides'] = available_rides
             return render(request, 'Driver/driver_search.html', context)
         else:
-            return redirect('pancake:driver_register')
+            return redirect('pancakeride:driver_register')
     else:
         #print('ppppp')
         return redirect('pancakeride:main_home')
@@ -192,6 +253,45 @@ def Driver_confirm(request, pk):
         ride_detail.driver = get_object_or_404(Driver, pk = request.user)
         ride_detail.status = 'cf'
         ride_detail.save()
+
+        owner = get_object_or_404(User, username = ride_detail.owner)
+        owner_email = owner.email
+        owner_name = owner.first_name + ' ' + owner.last_name
+
+        sharer = get_object_or_404(User, username = ride_detail.sharer)
+        sharer_email = sharer.email
+        sharer_name = sharer.first_name + ' ' + sharer.last_name
+
+        driver = get_object_or_404(User, username = ride_detail.driver)
+        driver_email = driver.email
+        driver_name = driver.first_name + ' ' + driver.last_name
+
+
+        send_mail(
+            'Ride Confirm for Pancake Ride',
+            'Hi '+ owner_name +'! Your ride has successfully confirmed by '+ driver_name+' in Pnacake Ride.\n',
+            'Pancake Ride',
+            [owner_email],
+            fail_silently=False,
+        )
+
+        send_mail(
+            'Ride Confirm for Pancake Ride',
+            'Hi '+ driver_name +'! You have successfully confirmed a ride in Pnacake Ride.\n',
+            'Pancake Ride',
+            [driver_email],
+            fail_silently=False,
+        )
+        
+        if sharer:
+            send_mail(
+                'Ride Confirm for Pancake Ride',
+                'Hi '+ sharer_name +'! Your share ride has successfully confirmed by '+ driver_name+' in Pnacake Ride.\n',
+                'Pancake Ride',
+                [sharer_email],
+                fail_silently=False,
+            )
+
         return redirect('pancakeride:main_home')
 
 def Driver_complete(request, pk):
